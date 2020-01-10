ef712d435ce27b314a7cf753f95f9a47e5235ca3
Vassilis Xanthopoulos <xanthopoulos.vassilis@gmail.com>
2020-01-10 03:05:11 +0200

    Implemented part of isBlocked and pulled some game logic into independent functions

--- chessboard.js
+++ chessboard.js
@@ -54,21 +54,101 @@ function canLongCastle(fromX, fromY, toX, toY) {
     }
 }
 
-function legalMove(fromX, fromY, toX, toY) {
+function commitMove(fromX, fromY, toX, toY) {
+    board[toY][toX] = board[fromY][fromX];
+    board[fromY][fromX] = EM;
+}
+
+function isBlocked(fromX, fromY, toX, toY, piece, color, movement) {
+    let dx, dy;
+    let tempY = fromY, tempX = fromX;
+    switch(movement) {
+        case 'diagonal':
+            dx = 1 - 2*(toX < fromX), dy = 1 - 2*(toY < fromY);
+
+            for(let i = 0; i < Math.abs(fromX - toX) - 1; ++i) {
+                tempX += dx;
+                tempY += dy;
+
+                console.log(tempX, tempY);
+
+                if(board[tempY][tempX] != EM) 
+                    return true;
+            }
+
+            return color == board[toY][toX].color;
+    
+        case 'horizontal':
+            if(fromY != toY) {
+                dy = 1 - 2*(fromY > toY);
+                
+                for(let i = 0; i < Math.abs(fromY - toY) - 1; ++i) {
+                    tempY += dy;
+    
+                    console.log(tempX, tempY);
+    
+                    if(board[tempY][tempX] != EM) 
+                        return true;
+                }
+    
+                return color == board[toY][toX].color;
+
+            } else {
+                dx = 1 - 2*(fromX > toX);
+
+                for(let i = 0; i < Math.abs(fromX - toX) - 1; ++i) {
+                    tempX += dx;
+    
+                    console.log(tempX, tempY);
+    
+                    if(board[tempY][tempX] != EM) 
+                        return true;
+                }
+    
+                return color == board[toY][toX].color;
+            }
+    }
+}
+
+function isInTurn(color) {
+    return color == turn; 
+}
+
+function isMoving(fromX, fromY, toX, toY) {
+    return fromX != toX || fromY != toY;
+}
+
+function isDiagonalMove(fromX, fromY, toX, toY) {
+    return Math.abs(fromX - toX) == Math.abs(fromY - toY);
+}
+
+function isStraightMove(fromX, fromY, toX, toY) {
+    return fromX == toX || fromY == toY;
+}
+
+function isKnightMove(fromX, fromY, toX, toY) {
+    return (Math.abs(fromX - toX) == 2 && Math.abs(fromY - toY) == 1) || (Math.abs(fromY - toY) == 2 && Math.abs(fromX - toX) == 1);
+}
+
+function isKingMove(fromX, fromY, toX, toY) {
+    return (Math.abs(fromX - toX) <= 1 && Math.abs(fromY - toY) <= 1) || canLongCastle(fromX, fromY, toX, toY) || canShortCastle(fromX, fromY, toX, toY);
+}
+
+function legalMove(fromX, fromY, toX, toY, piece, color) {
     let isLegal = false;
 
-    if (turn == board[fromY][fromX].color && (fromX != toX || fromY != toY) && (board[fromY][fromX].color != board[toY][toX].color)) {
-        switch(board[fromY][fromX].piece) {
+    if(isInTurn(color) && isMoving(fromX, fromY, toX, toY)) {
+        switch(piece) {
             case "pawn":
                 if(board[toY][toX] == EM){
-                    if(board[fromY][fromX].color == "white") {
+                    if(color == "white") {
                         isLegal = (fromX == toX) && (fromY == toY + 1) || ((fromY == toY + 2) && (fromX == toX) && fromY == 6);
                     } else {
                         isLegal = (fromX == toX) && (fromY == toY - 1) || ((fromY == toY - 2) && (fromX == toX) && fromY == 1);
                     }
                 } else {
-                    if(board[toY][toX].color != board[fromY][fromX].color) {
-                        if(board[fromY][fromX].color == "white") {           
+                    if(board[toY][toX].color != color) {
+                        if(color == "white") {           
                             isLegal = (fromY == toY + 1) && (Math.abs(fromX - toX) == 1)
                         } else {
                             isLegal = (fromY == toY - 1) && (Math.abs(fromX - toX) == 1)
@@ -77,19 +157,20 @@ function legalMove(fromX, fromY, toX, toY) {
                 }
                 break;
             case "bishop":
-                isLegal = (Math.abs(fromX - toX) == Math.abs(fromY - toY));
+                isLegal = isDiagonalMove(fromX, fromY, toX, toY) && !isBlocked(fromX, fromY, toX, toY, piece, color, 'diagonal');
                 break;
             case "knight":
-                isLegal = ((Math.abs(fromX - toX) == 2 && Math.abs(fromY - toY) == 1) || (Math.abs(fromY - toY) == 2 && Math.abs(fromX - toX) == 1));
+                isLegal = isKnightMove(fromX, fromY, toX, toY);
                 break;
             case "rook":
-                isLegal = (fromX == toX || fromY == toY);
+                isLegal = isStraightMove(fromX, fromY, toX, toY) && !isBlocked(fromX, fromY, toX, toY, piece, color, 'horizontal');
                 break;
             case "queen":
-                isLegal = (Math.abs(fromX - toX) == Math.abs(fromY - toY)) || (fromX == toX || fromY == toY);
+                isLegal = (isStraightMove(fromX, fromY, toX, toY) && !isBlocked(fromX, fromY, toX, toY, piece, color, 'horizontal')) ||
+                          (isDiagonalMove(fromX, fromY, toX, toY) && !isBlocked(fromX, fromY, toX, toY, piece, color, 'diagonal'));
                 break;
             case "king":
-                isLegal = (Math.abs(fromX - toX) <= 1 && Math.abs(fromY - toY) <= 1) || canLongCastle(fromX, fromY, toX, toY) || canShortCastle(fromX, fromY, toX, toY);
+                isLegal = isKingMove(fromX, fromY, toX, toY) && !isBlocked(fromX, fromY, toX, toY, piece, color, '');
                 break;
         }   
     }
@@ -98,10 +179,9 @@ function legalMove(fromX, fromY, toX, toY) {
 }
 
 function movePiece(fromX, fromY, toX, toY) {
-    if(legalMove(fromX, fromY, toX, toY)) {
-        board[toY][toX] = board[fromY][fromX];
-        board[fromY][fromX] = EM;
-        
+    if(legalMove(fromX, fromY, toX, toY, board[fromY][fromX].piece, board[fromY][fromX].color)) {
+        commitMove(fromX, fromY, toX, toY);
+
         if (turn == "white") {
             turn = "black";
         } else {
